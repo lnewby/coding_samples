1eef50ca1b8bf3de9445024e943c43c0de10f3f0
Lance Newby <lnewby@gmail.com>
2015-11-28 19:44:58 -0500

    Remove reliance on user to make sure array is sorted numerically or alphabetically. Add additional unit-test edge cases.

--- algorithms/js/binary_search.js
+++ algorithms/js/binary_search.js
@@ -1,44 +1,52 @@
 // Javascript
 
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
 /**
  * Binary Search:
- * - Pick number between 0 and 100
+ * - Enter a value to determine if it is in the array.
  */
 Array.prototype.binarySearch = function(target) {
   var min = 0;
   var max = this.length - 1;
 
+  // if guard to prevent array out of bounds error or invalid input value.
+  if(max < min) { return false; }
+
+  // Make sure array is in sorted order as required for binary search.
+  function numberCompare(a, b){ return a-b; }
+
+  var sortedArray = (typeof this[0] === 'number') ? this.sort(numberCompare) : this.sort();
+
   // Pick the middle element in the array
   var searchIndex = Math.floor(max / 2);
+  console.log('Array[' + searchIndex + '] = ' + sortedArray[searchIndex]);
 
-  if(max < 0) { return false; } // if guard to prevent array out of bounds error
-  console.log('Array[' + searchIndex + '] = ' + this[searchIndex]);
-
-  // If element pick is the target element return its location in the array
-  if(this[searchIndex] === target) {
+  // If element pick is the target element return its location in the array.
+  if(sortedArray[searchIndex] === target) {
     return true;
   }
 
   // If element picked is < target element set the new min element to one plus the current element picked
   // else set the new max to one less than the current element picked.
-  if(this[searchIndex] < target) {
+  if(sortedArray[searchIndex] < target) {
     min = searchIndex + 1;
   } else {
     max = searchIndex - 1;
   }
 
-  // check the new slice of the array recursively.
-  return this.slice(min, max+1).binarySearch(target);
+  // Check the new slice of the array recursively.
+  return sortedArray.slice(min, max + 1).binarySearch(target);
 }
 
 /**
  * Unit-Tests
  */
+ var primes = [31, 5, 7, 71, 11, 13, 19, 89, 97, 23, 29, 37, 3, 41, 43, 47, 2, 53, 59, 61, 67,  73, 79, 83, 17];
+
 console.assert(primes.binarySearch(-1) === false);
 console.assert(primes.binarySearch(0) === false);
 console.assert(primes.binarySearch(1) === false);
 console.assert(primes.binarySearch(2) === true);
 console.assert(primes.binarySearch(97) === true);
 console.assert(primes.binarySearch(100) === false);
+console.assert(primes.binarySearch(1000000) === false);
+console.assert(primes.binarySearch('string input edge case') === false);
